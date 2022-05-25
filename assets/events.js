f5f25319ef589fc9c896aaa3db0fc45ef4c5f6b5
dbBurzum <khalidsr@code.edu.az>
2022-05-25 16:29:33 +0400

    done

--- assets/events.js
+++ assets/events.js
@@ -1,31 +1,26 @@
-//npm install sweetalert --save
-//import swal from 'sweetalert';
-
 let input = document.getElementById("input")
-// input.addEventListener("focus", function(){
-//     console.log("focused");
-//     this.setAttribute("class", "def")
-//     swal("awesome");
-// })
-// input.addEventListener("blur", function(){
-//     console.log("blurred"); 
-//     swal("awesome");
-// })
+input.addEventListener("focus", function(){
+    console.log("focused");
+    this.setAttribute("class", "def")
+})
+input.addEventListener("blur", function(){
+    console.log("blurred"); 
+    swal("You wrote: " + input.value);
+})
 
-// input.addEventListener("keyup", function(){
-//     console.log(input.value);
-// })
+input.addEventListener("keyup", function(){
+    console.log(input.value);
+})
 
  let cars = document.getElementById("cars")
-// cars.addEventListener("change", function(){
-//     console.log(cars.value);
-// })
-// console.log(cars.value);
+cars.addEventListener("change", function(){
+    console.log(cars.value);
+})
+console.log(cars.value);
 
 let form = document.getElementById("form")
 let fname = document.getElementById("fname")
 let lname = document.getElementById("lname")
-form.onsubmit=function(){
- 
-
+document.onkeyup=function(e){
+    console.log(e.keyCode);
 }
